4432564c34a1a2708a4108c9ebe81926e18df8f9
Michael Betz <michibetz@gmail.com>
2020-03-15 01:46:45 -0700

    add platformIO based esp8266 firmware

---
+++ firmware/src/main.ino
@@ -0,0 +1,58 @@
+#include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+#define PIN_DATA 3
+#define N_LEDS 7
+#define UDP_PORT 2711
+
+Adafruit_NeoPixel strip(N_LEDS, PIN_DATA, NEO_GRB + NEO_KHZ800);
+
+WiFiUDP Udp;
+uint8_t udp_buff[4096];
+
+void setup() {
+	Serial.begin(115200);
+	strip.begin();
+
+	WiFi.begin("WIFI NAME", "WIFI PW");
+	strip.setPixelColor(0, 0xFF, 0, 0);
+	strip.show();
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+
+	strip.setPixelColor(0, 0, 0xFF, 0);
+	strip.show();
+
+	Serial.print("Connected! IP:");
+	Serial.println(WiFi.localIP());
+
+	Udp.begin(UDP_PORT);
+}
+
+void loop() {
+	int packetSize = Udp.parsePacket();
+	if (packetSize <= 0)
+		return;
+
+	int len = Udp.read(udp_buff, 4095);
+	if (len < 8)
+		return;
+
+	if (udp_buff[0] != 0)
+		return;
+
+	int led_offset = (udp_buff[1] << 8) | udp_buff[2];
+	int led_len = (udp_buff[3] << 8) | udp_buff[4];
+	uint8_t *p = &udp_buff[5];
+
+	for (int i=0; i<led_len; i++) {
+		strip.setPixelColor(led_offset, p[0], p[1], p[2]);
+		led_offset++;
+		p += 3;
+	}
+	strip.show();
+}
